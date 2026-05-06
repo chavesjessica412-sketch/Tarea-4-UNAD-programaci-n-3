9416dbea1f4b4db7cab362799226000561a79baf
SamtyJimenez19 <santiagijimenez813@gmail.com>
2026-05-06 21:17:07 +0200

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,7 +1,6 @@
 import datetime
 
-def registrar_evento(mensaje):
-    """Guarda eventos y errores en logs.txt con fecha y hora."""
+def log(tipo, mensaje):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open("logs.txt", "a", encoding="utf-8") as archivo:
-        archivo.write(f"[{timestamp}] {mensaje}\n")
+        archivo.write(f"[{timestamp}] {tipo}: {mensaje}\n")
